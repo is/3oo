be32f88968f55d0d0f719a2758ec71a80d3aca99
scaner <scaner@0610e858-b824-0410-92a0-e143797d3331>
2007-01-20 09:11:37 +0000

    - Log receiver.

---
+++ o3/Log.py
@@ -0,0 +1,26 @@
+import socket
+import time
+
+
+fout = file('/is/app/o3/log/o3.log', 'a')
+sin = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+sin.bind(('0.0.0.0', 50332))
+
+while True:
+	try:
+		buf = sin.recv(300)
+
+		log = '%s %s' % (time.strftime('%m%d %H:%M:%S'), buf[2:])
+
+		fout.write(log)
+		fout.write('\n')
+		fout.flush()
+
+		print log
+	except KeyboardInterrupt, e:
+		break
+
+	except:
+		pass
+sin.close()
+fout.close()
