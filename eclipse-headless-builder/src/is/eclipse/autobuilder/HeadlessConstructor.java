4c7000c220ed6034d6374ab92731410070f75986
scaner <scaner@0610e858-b824-0410-92a0-e143797d3331>
2009-01-22 08:49:24 +0000

    ci - Improve on headless builder.
     - showwarning option added.
     - compute project order before building.

--- eclipse-headless-builder/src/is/eclipse/autobuilder/HeadlessConstructor.java
+++ eclipse-headless-builder/src/is/eclipse/autobuilder/HeadlessConstructor.java
@@ -14,24 +14,26 @@ import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 
 public class HeadlessConstructor implements IApplication {
-  final String version = "0.0.0.2";
+  final String version = "0.0.0.3";
+  boolean showWarning = false;
   
 	@Override
 	public Object start(IApplicationContext appCtx) throws Exception {
 	  String arguments[] = (String[])appCtx.getArguments().get("application.args");
 	  String projectNames[] = null;
+	  
 	  for (String arg : arguments) {
 	    if (arg.startsWith("-projects=")) {
 	      projectNames = arg.substring("-projects=".length()).split(",");
 	    }
+	    if (arg.startsWith("-showwarning")) {
+	      showWarning = true;
+	    }
 	  }
 	  
 	  
-	  String projectList = (String)appCtx.getArguments().get("-projects");
-	  
-	  
 	  if (projectNames == null) {
-	    return null;
+	    return EXIT_OK;
 	  }
 	  
 	  System.out.format("Auto Headless Constructor Plugin: %s\n", version);
@@ -44,8 +46,6 @@ public class HeadlessConstructor implements IApplication {
 		IWorkspaceRoot wsRoot = ws.getRoot();
 		IPath wsPath = wsRoot.getLocation();
 		System.out.format("Workspace location is %s\n", wsPath.toString());
-		
-		// String projectNames[] = {"lib", "gs.common", "gs.framework"};
 		
 		for (String projectName : projectNames) {
 		  IProject proj = wsRoot.getProject(projectName);
@@ -61,18 +61,23 @@ public class HeadlessConstructor implements IApplication {
 		  }
 		}
 		
+		
 		System.out.format("There is %d projects\n", wsRoot.getProjects().length);
 		
 		// Begin auto build
 		System.out.println("Auto build workspace");
-		
 		ws.build(IncrementalProjectBuilder.CLEAN_BUILD, null);
-		ws.build(IncrementalProjectBuilder.FULL_BUILD, null);
+		
+		IWorkspace.ProjectOrder projOrder = ws.computeProjectOrder(wsRoot.getProjects());		
+		for (IProject project : projOrder.projects) {
+		  System.out.println("building " + project.getName() + " ...");
+		  project.build(IncrementalProjectBuilder.FULL_BUILD, null);
+		}
 
 		IMarker[] markers = wsRoot.findMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
 		if (markers == null)
 		{
-		  return new Integer(0);
+		  return EXIT_OK;
 		}
 		
 		int errorNum = 0;
@@ -80,10 +85,12 @@ public class HeadlessConstructor implements IApplication {
 		for (IMarker marker: markers) {
 		  int serverity = (Integer)marker.getAttribute(IMarker.SEVERITY);
 		  String serverityStr = null;
+		
 		  if (serverity == IMarker.SEVERITY_ERROR) {
 		    errorNum += 1;
 		    serverityStr = "ERROR";
-	      System.out.format("%s:%d %s - %s\n",
+	      
+		    System.out.format("%s:%d %s - %s\n",
 	          marker.getResource().getFullPath().toString().substring(1),
 	          marker.getAttribute(IMarker.LINE_NUMBER),
 	          serverityStr,
@@ -91,6 +98,14 @@ public class HeadlessConstructor implements IApplication {
 		  } else if(serverity == IMarker.SEVERITY_WARNING) {
 		    warnNum += 1;
 		    serverityStr = "WARN";
+		  
+		    if (showWarning) {
+	        System.out.format("%s:%d %s - %s\n",
+	            marker.getResource().getFullPath().toString().substring(1),
+	            marker.getAttribute(IMarker.LINE_NUMBER),
+	            serverityStr,
+	            marker.getAttribute(IMarker.MESSAGE));		      
+		    }
 		  } else if(serverity == IMarker.SEVERITY_INFO){
 		    serverityStr = "INFO";
 		  } else {
@@ -102,10 +117,10 @@ public class HeadlessConstructor implements IApplication {
 		
 		if (errorNum == 0)
 		{
-		  return new Integer(0);
+		  return EXIT_OK;
 		}
 		
-		return new Integer(2);
+		return new Integer(1);
 	}
 
 	@Override
